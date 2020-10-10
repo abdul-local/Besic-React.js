e37d99e040521530b980c79ce1406395ca8c683b
abdul-local <abdullah.hamzan@gmail.com>
2020-10-10 23:22:52 +0800

    Variabel ES6 dan Template Literals

---
+++ script.js
@@ -0,0 +1,39 @@
+
+//const name='codepolitan';
+
+//console.log(name);
+// menggunakan fungsi var
+
+//function belajarVar(){
+//    var pekerjaan='developer';
+//    var matappelajaran ='kimia reaksi'; // untuk global untuk var
+//    if(pekerjaan=='developer'){
+//        var matapelajaran='belajar javascript';
+//        console.log(matapelajaran);// belajar javascript
+//    }
+//    console.log(matapelajaran);// belajar javascript
+//}
+//
+//belajarVar();
+// meggunakan fungsi let
+
+//function belajarLet(){
+//    var pekerjaan='developer';
+//    var matappelajaran ='kimia reaksi'; // untuk global untuk var
+//    if(pekerjaan=='developer'){
+//        let matapelajaran='belajar javascript';
+//        console.log(matapelajaran);// belajar javascript
+//    }
+//    console.log(matappelajaran);// kimia reaksi
+//}
+//
+//belajarLet();
+var name='codepolitan';
+var alamat='jalan kembangkerang mamben daya no 26';
+
+// penggabungan string versi lama
+//console.log('kantor'+' '+name+' '+'berada di'+alamat);
+
+// penggabungan string dengan template literal
+
+console.log(`kantor ${name} berada di ${alamat}`);
