9f916b4992adc48e0296d6ed6c49abd2c9cb3863
abdul-local <abdullah.hamzan@gmail.com>
2020-10-11 13:16:18 +0800

    Fiture Array dan Object pada Es6

--- readme.md
+++ readme.md
@@ -4,3 +4,9 @@
 # Arrow Function
 1. untuk menyederhanakan sebuah function
 2. dengan penggunaan var variabel = {paramter}=>{condisi yang diinginkan};
+
+# Fitur Array dan Object pada ES6
+1. kita dapa menggunaka clone Array/obeject suapay mempermudah, gunanya untuk mengcopy yg sudah kita deklarasikan
+2. biasanya di tandai denga ...variavel
+3.Destructuring => ini berguna untuk mengambil bagian tertentu saja
+4.Object Literal Enchancment=>penyusuna object lebih singkat
--- script.js
+++ script.js
@@ -1,43 +1,94 @@
-// ES5
- function perkalian(a,b){
-     return a*b;
- }
+var angkaX=[1,2,3,4];
+//var angkaY=angkaX;
+//console.log(angkaY);
+//angkaY[0]=7;
+//console.log(angkaY);
+//console.log(angkaX);
 
-// arrow function
+// pada code di atas jelas memuculkan angka yg sama karena clone tidak bisa dengan sama dengan
+// spread syntax
 
-var perkalianArrow =(a,b)=>a*b;
+//var angkaY=[...angkaX];
+//angkaY[0]=7;
+//console.log(angkaY);
+//console.log(angkaX);
+//
+//var namaMahasiswaB=['abdul','hipzil','bianca'];
+//var namaMahasiswa=['andre','riza',...namaMahasiswaB,'pinka'];
+//console.log(namaMahasiswa);
+//
+//var perkalianlimaangka=(a,b,c,d,e)=>a*b*c*d*e;
+//var data=[1,2,3,4,5];
+//
+//// menggunakan cara manual
+//var hasil1=perkalianlimaangka(data[0],data[1],data[2],data[3],data[4]);
+//
+//// menggunakan spred sintax
+//var hasil=perkalianlimaangka(...data);
+//console.log(hasil);
+//console.log(hasil1);
+//var huruf=['a','b','c','d','e','f','g','h','i','j'];
+//var [hurufpertama]=huruf;
+//console.log(hurufpertama); // hasil a
+//var[hurufpertama,...sisaelement]=huruf;
+//console.log(sisaelement);
+//
+//
 
-var perkalianArrow2=(a,b)=>{
-    return a*b;
+// penggunaan destructuring pada object saja
+//var nilai ={
+//    andi:80,
+//    budi:90,
+//    zul:100,
+//    arman:70,
+//    dena:100,
+//}
+//
+//// mengambil nilai budi dengan cara baru
+//var {budi}=nilai;
+//console.log(budi);
+//
+//// mengamvil nilai dengan cara lama
+//console.log(nilai.budi);
+
+//Object Literal Enhancement
+var nama='abdul';
+var usia='18 tahun';
+var jeniskelamin='laki-laki';
+
+// deklarasikan dengan object dengan cara biasa
+var object={
+    nama:nama,
+    usia:usia,
+    jeniskelamin:jeniskelamin,
+    hobby:'leraning program',
 }
-// memanggil fungsi
-console.log(perkalian(5,10));
-console.log(perkalianArrow(5,10));
-console.log(perkalianArrow2(5,10));
-
-function sayHellow(){
-    console.log('hellow, Iam codepolitan')
-    
+// deklarasikan oject dengan OLE
+var object1={
+    nama,
+    usia,
+    jeniskelamin,
+    hobby:'dangdutan'
 }
 
-// codingan diatasa tanpa paramter dapat diganti dengan
- var newsayHellow=()=> {
-    console.log('hellow,Iam codepolitan')
-}
+console.log(object);
+console.log(object1);
 
-function greeting(guestname){
-    return `Welcome back to Codepolitan,${guestname}`
-}
-// kode diatas bisa diganti
-var newGriting=guesname=>`welcome back Codepolitan, ${guestname}`;
-newsayHellow();
-var angka =[1,2,3,4]
 
-var angkaBaru = angka.map(function(element){
-    return element*2;
-})
 
-// kode di atas bisa diganti dengan
 
-var angkabarunew = angka.map(elemenet=>element*2);
-console.log(angkaBaru);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
