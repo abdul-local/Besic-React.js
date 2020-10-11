f3ae3e3e4a33a5482e08f674b12d3a867e5c7cd0
abdul-local <abdullah.hamzan@gmail.com>
2020-10-11 12:04:27 +0800

    Arrow Function

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Variabel ES6 dan Tempalte Literal
+1. pengggunakaan var dengan let var di pakai untuk cakupan global sedangkan let dipakai di dalam block code itu sendiri
+2. untuk literal kita langsung menggunakan ${varivel} untuk mencetak nilainya tidak menggunakan cara dahulu
+# Arrow Function
+1. untuk menyederhanakan sebuah function
+2. dengan penggunaan var variabel = {paramter}=>{condisi yang diinginkan};
--- script.js
+++ script.js
@@ -1,39 +1,43 @@
+// ES5
+ function perkalian(a,b){
+     return a*b;
+ }
 
-//const name='codepolitan';
+// arrow function
 
-//console.log(name);
-// menggunakan fungsi var
+var perkalianArrow =(a,b)=>a*b;
 
-//function belajarVar(){
-//    var pekerjaan='developer';
-//    var matappelajaran ='kimia reaksi'; // untuk global untuk var
-//    if(pekerjaan=='developer'){
-//        var matapelajaran='belajar javascript';
-//        console.log(matapelajaran);// belajar javascript
-//    }
-//    console.log(matapelajaran);// belajar javascript
-//}
-//
-//belajarVar();
-// meggunakan fungsi let
+var perkalianArrow2=(a,b)=>{
+    return a*b;
+}
+// memanggil fungsi
+console.log(perkalian(5,10));
+console.log(perkalianArrow(5,10));
+console.log(perkalianArrow2(5,10));
 
-//function belajarLet(){
-//    var pekerjaan='developer';
-//    var matappelajaran ='kimia reaksi'; // untuk global untuk var
-//    if(pekerjaan=='developer'){
-//        let matapelajaran='belajar javascript';
-//        console.log(matapelajaran);// belajar javascript
-//    }
-//    console.log(matappelajaran);// kimia reaksi
-//}
-//
-//belajarLet();
-var name='codepolitan';
-var alamat='jalan kembangkerang mamben daya no 26';
+function sayHellow(){
+    console.log('hellow, Iam codepolitan')
+    
+}
 
-// penggabungan string versi lama
-//console.log('kantor'+' '+name+' '+'berada di'+alamat);
+// codingan diatasa tanpa paramter dapat diganti dengan
+ var newsayHellow=()=> {
+    console.log('hellow,Iam codepolitan')
+}
 
-// penggabungan string dengan template literal
+function greeting(guestname){
+    return `Welcome back to Codepolitan,${guestname}`
+}
+// kode diatas bisa diganti
+var newGriting=guesname=>`welcome back Codepolitan, ${guestname}`;
+newsayHellow();
+var angka =[1,2,3,4]
 
-console.log(`kantor ${name} berada di ${alamat}`);
+var angkaBaru = angka.map(function(element){
+    return element*2;
+})
+
+// kode di atas bisa diganti dengan
+
+var angkabarunew = angka.map(elemenet=>element*2);
+console.log(angkaBaru);
